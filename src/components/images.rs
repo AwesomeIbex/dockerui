9c331213dc2d733e05d5b6fa9f66903ece9e6eae
Donovan Dall <donovandall@icloud.com>
2020-11-13 14:45:31 +0000

    make it prettier

--- src/components/images.rs
+++ src/components/images.rs
@@ -4,12 +4,12 @@ use bollard::service::ContainerSummaryInner;
 use itertools::Itertools;
 use tui::backend::Backend;
 use tui::Frame;
-use tui::layout::{Alignment, Rect};
+use tui::layout::{Rect};
 use tui::style::{Color, Modifier, Style};
 use tui::text::{Span, Spans};
-use tui::widgets::{Block, Borders, List, ListItem, Paragraph};
+use tui::widgets::{Block, Borders, List, ListItem};
 
-use crate::components::{DrawableComponent, MutableDrawableComponent};
+use crate::components::{MutableDrawableComponent};
 use crate::components::main_app::MainApp;
 use crate::components::util::StatefulList;
 
@@ -20,28 +20,10 @@ pub struct Images {
 
 impl MutableDrawableComponent for Images {
     fn draw<B: Backend>(&mut self, f: &mut Frame<B>, rect: Rect, app: &MainApp) -> Result<(), Error> {
-        let items: Vec<ListItem> = app.images.clone()
-            .iter()
-            .map(|i| {
-                let summary = i.clone();
-                let images = summary.labels.get_key_value("name");
+        let mut names = Images::filter_names(app);
+        names.dedup();
 
-                match images {
-                    None => vec![],
-                    Some(value) => {
-                        let name = value.1.clone();
-                        let mut lines = vec![];
-
-                        lines.push(Spans::from(Span::styled(
-                            name,
-                            Style::default().add_modifier(Modifier::ITALIC),
-                        )));
-                        lines
-                    }
-                }
-            }).dedup()
-            .map(|lines| ListItem::new(lines).style(Style::default().fg(Color::Black).bg(Color::White)))
-            .collect();
+        let items = Images::map_to_list_items(&names);
 
         //TODO items is 38 here :/
         let items = List::new(items)
@@ -57,11 +39,52 @@ impl MutableDrawableComponent for Images {
     }
 }
 
+
 impl Images {
     pub fn new() -> Images {
         Images {
             selected: 0,
             items: StatefulList::new(),
         }
+    }
+}
+
+impl Images {
+    fn filter_names(app: &MainApp) -> Vec<String> {
+        let names: Vec<String> = app.images.clone()
+            .iter()
+            .map(|i| {
+                let summary = i.clone();
+                let images = summary.labels.get_key_value("name");
+
+                match images {
+                    None => String::new(),
+                    Some(value) => {
+                        value.1.clone()
+                    }
+                }
+            })
+            .filter(|name| !name.is_empty())
+            .collect();
+        names
+    }
+}
+
+impl Images {
+    fn map_to_list_items(names: &Vec<String>) -> Vec<ListItem> {
+        let items: Vec<ListItem> = names
+            .iter()
+            .map(|name| {
+                vec![(Spans::from(Span::styled(
+                    name,
+                    Style::default().add_modifier(Modifier::ITALIC),
+                )))]
+            })
+            .map(|mut lines| {
+                lines.dedup();
+                ListItem::new(lines).style(Style::default().fg(Color::Red))
+            })
+            .collect();
+        items
     }
 }
